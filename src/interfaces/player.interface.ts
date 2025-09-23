d2e5917fced6c7a1c301f71767f4d6aa740cddc1
Dase <104153237+SebastianG03@users.noreply.github.com>
2025-09-23 01:31:38 +0000

    Pagina de detalles creada

---
+++ src/interfaces/player.interface.ts
@@ -0,0 +1,17 @@
+export interface Player {
+    id: number,
+    name: string,
+    lastname: string,
+    position: string,
+    team: string,
+    shirtNumber: number,
+    minutesPlayed: number,
+    goalsNumber: number,
+    goalAverage: number,
+    soccerGoalShotsPercentage: number,
+    soccerGoalShots: number,
+    shotsNumber: number,
+    coveredDistance: number,
+    playerImageUrl: string,
+    heatmapUrl: string
+}
